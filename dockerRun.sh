526f66a3e131701a29bdb6154b66bca552bd85ff
Robert Smith - ASUS Dtop <konokhead@gmail.com>
2018-02-14 08:29:56 -0600

    Working on docker build/run

---
+++ dockerRun.sh
@@ -0,0 +1,2 @@
+#docker run -i -t resmith/mongo-base2
+docker run -d -p 27017:27017 resmith/mongo-base2
--- runDocker.sh
+++
@@ -1,2 +0,0 @@
-#docker run -i -t resmith/mongo-base2
-docker run -d -p 3001:27017 resmith/mongo-base2
